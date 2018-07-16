ddba164923820bb286c4418483398f39779d84fe
Ranjan Shrestha <ranjan@sharingapples.com>
2018-07-16 11:16:18 +0545

    fix normalized methods export

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import PartialReducer from './PartialReducer';
 import shallowEqual from './shallowEqual';
 import observe from './observe';
 import observeStore from './observeStore';
-import normalized from './normalized';
+import * as normalized from './normalized';
 
 export {
   PartialReducer,
